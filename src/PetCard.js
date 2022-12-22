0fe0659fa7188f2cb3fa16c034f619739d0ad949
hoonkim <hoonk9387@gmail.com>
2022-12-21 21:40:42 -0600

    In petcard component call map on props.snack array and return new array of li strings to be rendered in each petcard component.

--- src/PetCard.js
+++ src/PetCard.js
@@ -1,11 +1,23 @@
 import React from 'react'
 
 function PetCard(props) {
-  console.log(props.snack)
+  // console.log(props.snack)
+
+  // Loop through array using map. Returns new array.
+  const snackArr = props.snack.map((strSnack) => {
+    return <li>{strSnack}</li>
+  })
+
+  console.log("snackArr", snackArr)
+
   return (
     <div className="card">
       <h2>{props.name}</h2>
       <img src={props.image} alt={props.name} />
+      <h3>Snacks</h3>
+      <ul>
+        {snackArr}
+      </ul>
     </div>
   )
 }
